dd98fa197a7b2f2de2897b5312bdbc6ec05b39c6
Trevor Simpkin <TrevorSimpkin@gmail.com>
2019-06-03 14:53:37 -0400

    Scroller website;

--- application.go
+++ application.go
@@ -1,3 +1,4 @@
+// Package main listens and serves on port 3000
 package main
 
 import (
@@ -10,14 +11,22 @@ import (
 func main() {
     port := os.Getenv("PORT")
         if port == "" {
-            port = "5000"
+            port = "3000"
         }
 
-        f, _ := os.Create("/var/log/golang/golang-server.log")
+        f,err := os.Create("/home/administrator/go/log/log.txt")
+        if err != nil{
+            log.Println(err)
+        }
         defer f.Close()
         log.SetOutput(f)
-
+        //certManager := autocert.Manager{
+        //    Prompt:     autocert.AcceptTOS,
+        //    HostPolicy: autocert.HostWhitelist("localhost"),
+        //    Cache:      autocert.DirCache("certs"),
+        //}
         const indexPage = "public/index.gohtml"
+        http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
             if r.Method == "POST" {
                 if buf, err := ioutil.ReadAll(r.Body); err == nil {
@@ -28,13 +37,21 @@ func main() {
                 http.ServeFile(w, r, indexPage)
             }
         })
-
         http.HandleFunc("/scheduled", func(w http.ResponseWriter, r *http.Request){
             if r.Method == "POST" {
             log.Printf("Received task %s scheduled at %s\n", r.Header.Get("X-Aws-Sqsd-Taskname"), r.Header.Get("X-Aws-Sqsd-Scheduled-At"))
             }
         })
-
+        //server := &http.Server{
+        //    Addr: ":https",
+        //    TLSConfig: &tls.Config{
+        //        GetCertificate: certManager.GetCertificate,
+        //    },
+        //}
         log.Printf("Listening on port %s\n\n", port)
-        http.ListenAndServe(":"+port, nil)
+        //http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+        err = http.ListenAndServe(":"+port, nil)
+        //if false {
+        //    log.Fatal(server.ListenAndServeTLS("", ""))
+        //}  //Key and cert are coming from Let's Encrypt
 }
